fdee323d139bec93c6479712f91d482ba906b918
bhanu256 <bhanuphani100@gmail.com>
2024-08-28 19:20:33 +0530

    Even Odd Locks

--- MultiThreading/src/EvenOdd.java
+++ MultiThreading/src/EvenOdd.java
@@ -1,3 +1,7 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class EvenOdd {
 
     public void execute () {
@@ -19,38 +23,72 @@ public class EvenOdd {
     }
 }
 
+//class Common {
+//    private int counter = 1;
+//    private int upperLimit;
+//    private Object object = new Object();
+//
+//    public Common (int upperLimit) {
+//        this.upperLimit = upperLimit;
+//    }
+//
+//    public void printEven () throws InterruptedException {
+//        synchronized (object) {
+//            while (counter <= upperLimit) {
+//                if (counter % 2 == 1) {
+//                    object.wait();
+//                }
+//
+//                System.out.println(counter++);
+//                object.notifyAll();
+//            }
+//        }
+//    }
+//
+//    public synchronized void printOdd () throws InterruptedException {
+//        synchronized (object) {
+//            while (counter <= upperLimit) {
+//                if (counter % 2 == 0) {
+//                    object.wait();
+//                }
+//
+//                System.out.println(counter++);
+//                object.notifyAll();
+//            }
+//        }
+//    }
+//}
+
 class Common {
-    private int counter = 1;
+    private int counter = 0;
     private int upperLimit;
-    private Object object = new Object();
+    Lock lock = new ReentrantLock();
+    Condition cond = lock.newCondition();
 
     public Common (int upperLimit) {
         this.upperLimit = upperLimit;
     }
 
     public void printEven () throws InterruptedException {
-        synchronized (object) {
-            while (counter <= upperLimit) {
-                if (counter % 2 == 1) {
-                    object.wait();
-                }
+        while (counter <= upperLimit) {
+            lock.lock();
+            if (counter % 2 == 1) cond.await();
 
-                System.out.println(counter++);
-                object.notifyAll();
-            }
+            System.out.println(counter++);
+            cond.signalAll();
+            lock.unlock();
         }
     }
 
-    public synchronized void printOdd () throws InterruptedException {
-        synchronized (object) {
-            while (counter <= upperLimit) {
-                if (counter % 2 == 0) {
-                    object.wait();
-                }
+    public void printOdd () throws InterruptedException {
+        while (counter <= upperLimit) {
+            lock.lock();
+            if (counter % 2 == 0) cond.await();
 
-                System.out.println(counter++);
-                object.notifyAll();
-            }
+            System.out.println(counter++);
+            cond.signalAll();
+            lock.unlock();
         }
     }
+
 }
--- MultiThreading/src/Main.java
+++ MultiThreading/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        ProducerConsumer obj = new ProducerConsumer();
+        EvenOdd obj = new EvenOdd();
         obj.execute();
     }
 }
